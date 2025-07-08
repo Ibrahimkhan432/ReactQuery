4dae433300e559c025bc3eb99dde8dd71c7c1e2d
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-07-08 12:05:59 +0500

    search and control response through abortControl

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,36 +6,55 @@ function App() {
   const [products, setProducts] = useState([])
   const [error, setError] = useState(false)
   const [loading, setLoading] = useState(false)
+  const [search, setSearch] = useState('')
 
   // iife
   useEffect(() => {
+
+    // Cleanup function to abort the request if the component unmounts
+    const controller = new AbortController()
+
+      ;   // if remove (;) you will receive a error(intermediate value is not a function) 
+
     (async () => {
       try {
         setError(false)
         setLoading(true)
-        const response = await axios.get('/api/products')
+        const response = await axios.get('/api/products?search=' + search,
+          { signal: controller.signal } // Pass the abort signal to axios
+        )
         console.log(response.data);
         setProducts(response.data)
         setLoading(false)
       } catch (error) {
+
+        if (axios.isCancel(error)) {
+          console.log('Request canceled', error.message);
+          return; // Exit if the request was canceled
+        }
         setError(true)
         console.error('Error fetching products:', error);
         setLoading(false)
+
       }
+      return () => {
+        controller.abort() // Abort the request when the component unmounts
+      }
+
     })()
-  }, [])
-
-  if (error) {
-    return <h1>Error fetching products</h1>
-  }
-
-if (loading) {
-    return <h1>Loading...</h1>
-  }
+  }, [search])
 
   return (
     <><h1>App jsx</h1>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error fetching products</h1>}
       <h2>products are {products.length}</h2>
+
+      {/* search product name which match with your product name */}
+      <input type="text" placeholder="Search products"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
     </>
   )
 }
